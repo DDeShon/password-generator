a0c2fc06689e86e229c4b326644ff9307704a4e3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-08 06:08:58 -0500

    added createPassword function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const program = require("commander");
+const log = console.log();
 
 program.version("1.0.0").description("Random Password Generator");
 
 program
   .option("-l, --length <number>", "length of password", "8")
-  .option("-s, --save", "save password to clipboard")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "create password without numbers")
+  .option("-ns, --no-symbols", "create password without symbols")
   .parse();
 
-console.log(program.opts());
+const { length, save, numbers, symbols } = program.opts();
+
+// Get generated password
+const generatedPassword = createPassword(length, numbers, symbols);
+
+// Output generated password
+log(generatedPassword);
