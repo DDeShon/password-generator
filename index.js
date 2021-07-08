61886ab4703c2a1712d3132126cf9b9fefe9f167
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-08 05:56:12 -0500

    working on options

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const program = require("commander");
 program.version("1.0.0").description("Random Password Generator");
 
 program
-  .command("generate")
-  .action(() => {
-    console.log("Generated");
-  })
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to clipboard")
   .parse();
+
+console.log(program.opts());
